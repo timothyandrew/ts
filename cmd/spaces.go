bb8060587b1b71e6ba3f67ea532442427a808f5a
Timothy Andrew <mail@timothyandrew.net>
2020-12-01 10:28:06 +0530

    Allow switching to space

--- cmd/spaces.go
+++ cmd/spaces.go
@@ -23,11 +23,45 @@ type Space struct {
 	Id        uint
 }
 
+// listCmd Args
 var (
 	OnlyNamed bool
 	JSON      bool
 	Alfred    bool
 )
+
+// switchCmd Args
+var (
+	SwitchToSpaceNumber int
+	SwitchToSpaceName   string
+)
+
+func listSpaces() []Space {
+	var spaces []Space
+	var displays = ts2.DisplayList()
+
+	for _, display := range displays {
+		var spaceCount = ts2.NumberOfSpacesOnDisplay(display.ID)
+
+		for i := uint(1); i <= spaceCount; i++ {
+			var name = ts2.CustomNameForSpaceNumberOnDisplay(i, display.ID)
+
+			if OnlyNamed && name == "" {
+				continue
+			}
+
+			name = ts2.SpaceNameForSpaceNumberOnDisplay(i, display.ID)
+
+			spaces = append(spaces, Space{
+				Name:      name,
+				DisplayID: display.ID,
+				Id:        i,
+			})
+		}
+	}
+
+	return spaces
+}
 
 var spacesCmd = &cli.Command{
 	Use:   "spaces",
@@ -38,28 +72,7 @@ var listCmd = &cli.Command{
 	Use:   "list",
 	Short: "List all spaces",
 	Run: func(cmd *cli.Command, args []string) {
-		var spaces []Space
-		var displays = ts2.DisplayList()
-
-		for _, display := range displays {
-			var spaceCount = ts2.NumberOfSpacesOnDisplay(display.ID)
-
-			for i := uint(1); i <= spaceCount; i++ {
-				var name = ts2.CustomNameForSpaceNumberOnDisplay(i, display.ID)
-
-				if OnlyNamed && name == "" {
-					continue
-				}
-
-				name = ts2.SpaceNameForSpaceNumberOnDisplay(i, display.ID)
-
-				spaces = append(spaces, Space{
-					Name:      name,
-					DisplayID: display.ID,
-					Id:        i,
-				})
-			}
-		}
+		var spaces = listSpaces()
 
 		if JSON {
 			var output, err = json.Marshal(spaces)
@@ -90,9 +103,35 @@ var listCmd = &cli.Command{
 	},
 }
 
+var switchCmd = &cli.Command{
+	Use:   "switch",
+	Short: "Switch to a space",
+	Run: func(cmd *cli.Command, args []string) {
+		var spaces = listSpaces()
+
+		// TODO: Improve this using better lookup, but I'm guessing that iterating over
+		// ~16 spaces is going to be close enough to O(1) that it doesn't really matter.
+		for _, space := range spaces {
+			if SwitchToSpaceNumber != -1 && space.Id == uint(SwitchToSpaceNumber) {
+				ts2.MoveToSpaceOnDisplay(space.Id, space.DisplayID)
+				return
+			}
+
+			if SwitchToSpaceName != "" && space.Name == SwitchToSpaceName {
+				ts2.MoveToSpaceOnDisplay(space.Id, space.DisplayID)
+				return
+			}
+		}
+	},
+}
+
 func init() {
 	spacesCmd.AddCommand(listCmd)
 	listCmd.Flags().BoolVarP(&OnlyNamed, "only-named", "n", false, "Only display named spaces")
 	listCmd.Flags().BoolVarP(&JSON, "json", "j", false, "JSON output")
 	listCmd.Flags().BoolVarP(&Alfred, "alfred", "a", false, "Output for Alfred's script filter")
+
+	spacesCmd.AddCommand(switchCmd)
+	switchCmd.Flags().IntVarP(&SwitchToSpaceNumber, "number", "i", -1, "Switch to a space by number (starts at 1; doesn't currently support multiple displays)")
+	switchCmd.Flags().StringVarP(&SwitchToSpaceName, "name", "n", "", "Switch to a space by name")
 }
